82248e46ee41c77506bb31cedf14a992a96082d2
mohammad charipour <mohammd.ch81m@gmail.com>
2025-05-09 16:07:19 +0330

    add tests directory in account app

---
+++ account/tests/factory.py
@@ -0,0 +1,7 @@
+import factory
+import uuid
+from factory.django import DjangoModelFactory
+from datetime import datetime
+from news.models.news_model import News
+from news.models.tag_model import Tag
+
